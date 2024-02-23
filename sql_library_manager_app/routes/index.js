9d7d80591d43d9b7a9ec50ab17112ae931d46f3e
Alan McClenaghan <AlanM@Alans-MBP-2.cust.communityfibre.co.uk>
2024-02-23 21:41:38 +0000

    Required fields and forms

--- sql_library_manager_app/routes/index.js
+++ sql_library_manager_app/routes/index.js
@@ -42,22 +42,42 @@ router.get('/books/new', asyncHandler(async (req, res) => {
 
 // post /books/new - Posts a new book to the database
 router.post('/books/new', asyncHandler(async (req, res) => {
-  const book = await Book.create(req.body);
-  // console.log(req.body);
-  res.redirect('/books');
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect('/books');
+  } catch (error) {
+      if(error.name === "SequelizeValidationError") { // checking the error
+        book = await Book.build(req.body);
+        res.render("new-book", { book, errors: error.errors, title: "New Book" })
+      } else {
+        throw error; // error caught in the asyncHandler's catch block
+      }  
+  }
 }));
 
 // get /books/:id - Shows book detail form
 router.get('/books/:id', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
-  res.render('update-book', { book, title: book.title });
+  res.render('update-book', { book, title: "Update Book" });
 }));
 
 // post /books/:id - Updates book info in the database
 router.post('/books/:id', asyncHandler(async (req, res) => {
-  const book = await Book.findByPk(req.params.id);
-  await book.update(req.body);
-  res.redirect('/');
+  let book;
+  try {
+    book = await Book.findByPk(req.params.id);
+    await book.update(req.body);
+    res.redirect('/');
+  } catch (error) {
+      if(error.name === "SequelizeValidationError") { // checking the error
+        book = await Book.build(req.body);
+        book.id = req.params.id; // make sure correct book gets updated
+        res.render("update-book", { book, errors: error.errors, title: "Update Book"  })
+      } else {
+        throw error; // error caught in the asyncHandler's catch block
+      }  
+  }
 }));
 
 /* post /books/:id/delete - Deletes a book. Be careful, this can’t be undone. 
